e6db68d7cb48664c3b23fbd85e40f64ed854a67a
mariayasar <m.dubrovskaya@gmail.com>
2020-02-10 21:24:51 -0800

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-from setuptools import find_packages, setup
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
+"""
+lambdata - a collection of data science helper functions for lambda school
+"""
+import setuptools
 
 REQUIRED = [
-
-"numpy",
-"pandas"
-
+    "numpy",
+    "pandas"
 ]
 
-setup(
-    name="mypackage",
-    version="0.1",
-    author="Maria Yasar",
-    author_email="hello@mariayasar.com",
-    description="Example package for lambda school DS Unit 3",
-    long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    license="MIT",
-    url="https://github.com/mariayasar/mypackage",
-    keywords="data science",
-    packages=find_packages() # ["my_lambdata"]
-)
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+    name="lambdata-yasar",
+    version = "0.1.1",
+    author = "mariayasar",
+    description = "a collection of data science helper functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://lambdaschool.com/courses/data-science",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires = REQUIRED,
+    classifiers=["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+    )
