61053072712e23b4f9862c0620ea5c3c04946e36
mariayasar <m.dubrovskaya@gmail.com>
2020-02-10 16:04:41 -0800

    add poc utility files

---
+++ lambdata_mariayasar/__init__.py
@@ -0,0 +1,13 @@
+"""
+lambdata - a collection of datascience helper functions 
+
+"""
+
+import pandas as pd 
+import numpy as np 
+
+#sample code 
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS= pd.DataFrame(np.zeros(50))
+
---
+++ lambdata_mariayasar/df_utils.py
@@ -0,0 +1,9 @@
+"""
+
+utility functions for working with dataframes 
+
+"""
+
+import pandas as pd 
+
+TEST_DF=pd.DataFrame([1,2,3,5])
